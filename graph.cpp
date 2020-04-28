c8816fb3431896ae1d8c97d6f46a30dce0008724
Rohit <rohit5822540@gmail.com>
2020-04-28 19:55:26 +0530

    BFS of a undirected graph

--- graph.cpp
+++ graph.cpp
@@ -4,26 +4,41 @@ void addedge(vector<int> adj[],int u,int v){
 	adj[u].push_back(v);
 	adj[v].push_back(u);
 }
-void printgraph(vector<int> adj[],int V){
-	for(int v=0;v<V;v++){
-		cout<<"\nAdjacency list of vertex "<<v<<" :"<<" head";
-		for(auto x:adj[v]){
-			cout<<"->"<<x;
+
+void BFS(vector<int> adj[],int V){
+	bool visited[V+1];
+	int i;
+	for(i=0;i<=V;i++){
+		visited[i]=false;
+	}
+	queue<int>q;
+	int s=1;
+	visited[s]=true;
+	q.push(s);
+	while(!q.empty()){
+		int root=q.front();
+		q.pop();
+		cout<<root<<" ";
+		for(auto x:adj[root]){
+			if(visited[x]==false){
+				q.push(x);
+				visited[x]=true;
+			}
 		}
-		cout<<endl;
 	}
 }
 
 int main(){
-	int V=5;
-	vector<int> adj[V];
-	addedge(adj,0,1);
-	addedge(adj,0,4);
+	int V=6;
+	vector<int> adj[V+1];
 	addedge(adj,1,2);
 	addedge(adj,1,3);
-	addedge(adj,1,4);
-	addedge(adj,2,3);
-	addedge(adj,3,4);
-	printgraph(adj,V);
+	addedge(adj,2,4);
+	addedge(adj,2,5);
+	addedge(adj,3,5);
+	addedge(adj,4,5);
+	addedge(adj,4,6);
+	addedge(adj,5,6);
+	BFS(adj,V);
 	return 0;
 }
