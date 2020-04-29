2f969bbec13046a6a13aff83959eb66bf11af5e5
Rohit <rohit5822540@gmail.com>
2020-04-29 11:38:31 +0530

    Shortest path in the unweighted and undirected graph

--- graph.cpp
+++ graph.cpp
@@ -4,46 +4,49 @@ void addedge(vector<int> adj[],int u,int v){
 	adj[u].push_back(v);
 	adj[v].push_back(u);
 }
-int search(vector<int> adj[],bool visited[],int i,int parent){
-	visited[i]=true;
-	for(auto x:adj[i]){
-		if(visited[x]==false){
-			if(search(adj,visited,x,i))	
-				return 1;
-		}
-		else if(x!=parent){
-			return 1;
-		}
-	}
-	return 0;
-}
-int detectcycle(vector<int>adj[],int V){
-	int i;
-	bool visited[V+1];
-	for(i=1;i<=V;i++){
+void shortestpath(vector<int> adj[],int source,int dest,int V){
+	int arr[V],i;
+	bool visited[V];
+	for(i=0;i<V;i++){
+		arr[i]=INT_MAX;
 		visited[i]=false;
 	}
-	for(i=1;i<=V;i++){
-		if(visited[i]==false){
-			if(search(adj,visited,i,-1))
-				return 1;
+	queue<int>q;
+	arr[source]=0;
+	q.push(source);
+	visited[source]=true;
+	while(!q.empty()){
+		int root=q.front();
+		q.pop();
+		for(auto x:adj[root]){
+			if(visited[x]==false){
+				if(arr[x]>arr[root]+1){
+					visited[x]=true;
+					q.push(x);
+					arr[x]=arr[root]+1;
+				}
+			}
 		}
 	}
-	return 0;
+	for(i=0;i<V;i++){
+		cout<<i<<" : "<<arr[i]<<endl;
+	}
+	cout<<"Shortest path from "<<source<<" to "<<dest<<" is "<<arr[dest];
 }
 int main(){
-	int V=6;
-	vector<int> adj[V+1];
+	int V=8;
+	vector<int>adj[V];
+	addedge(adj,0,1);
+	addedge(adj,0,3);
 	addedge(adj,1,2);
-	addedge(adj,1,3);
-	addedge(adj,2,4);
-	addedge(adj,2,5);
 	addedge(adj,3,4);
-	if(detectcycle(adj,V)){
-		cout<<"Cycle detected"<<endl;
-	}
-	else{
-		cout<<"Cycle not detected"<<endl;
-	}
+	addedge(adj,3,7);
+	addedge(adj,4,5);
+	addedge(adj,4,6);
+	addedge(adj,4,7);
+	addedge(adj,5,6);
+	addedge(adj,6,7);
+	int source=0,dest=7;
+	shortestpath(adj,source,dest,V);
 	return 0;
 }
